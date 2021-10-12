ae334b5f667f948017324371f07fbf07a87a0143
Arunsh93 <Arunhosur93@gmail.com>
2021-10-12 23:35:53 +0530

    Validating Emails Ids by using MS Test

--- UserResistrationTest/UnitTest1.cs
+++ UserResistrationTest/UnitTest1.cs
@@ -86,5 +86,33 @@ namespace UserResistrationTest
             //Assertion
             Assert.AreEqual(ExpectedName, ActualName);
         }
+        [TestMethod]
+        public void TestSampleEmailIds()
+        {
+            UserResistrationValidation userResistration = new UserResistrationValidation();
+
+            //Arrange
+            string Email1 = "arun@yahoo.com";
+            string Email2 = "arun-100@yahoo.com";
+            string Email3 = "abc.100@yahoo.com";
+            string Email4 = "abc111@abc.com";
+            string Email5 = "abc-100@abc.com.ac";
+
+            bool ExpectedData = true;
+
+            //Action
+            bool ActualData1 = userResistration.ValidateSampleEmailIds(Email1);
+            bool ActualData2 = userResistration.ValidateSampleEmailIds(Email2);
+            bool ActualData3 = userResistration.ValidateSampleEmailIds(Email3);
+            bool ActualData4 = userResistration.ValidateSampleEmailIds(Email4);
+            bool ActualData5 = userResistration.ValidateSampleEmailIds(Email5);
+
+            //Assert
+            Assert.AreEqual(ExpectedData, ActualData1);
+            Assert.AreEqual(ExpectedData, ActualData2);
+            Assert.AreEqual(ExpectedData, ActualData3);
+            Assert.AreEqual(ExpectedData, ActualData4);
+            Assert.AreEqual(ExpectedData, ActualData5);
+        }
     }
 }
