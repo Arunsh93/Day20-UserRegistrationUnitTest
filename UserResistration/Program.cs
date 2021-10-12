2e44e8834c2c704a3753d0ca51297be4fbfe3158
Arunsh93 <Arunhosur93@gmail.com>
2021-10-12 15:54:32 +0530

    Validate user first name using MSTest

---
+++ UserResistration/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UserResistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Console.WriteLine("*****Welcome to User Resistration*****");
+                UserResistrationValidation userResistration = new UserResistrationValidation();
+                Console.Write("Enter First Name: ");
+                string FirstName = Console.ReadLine();
+                bool Result = userResistration.ValidateFirstName(FirstName);
+
+                if (Result == true)
+                {
+                    Console.WriteLine("Valid Name!");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Name!");
+                }
+            
+        }
+    }
+}
---
+++ UserResistration/UserResistrationValidation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserResistration
+{
+    public class UserResistrationValidation
+    {
+        public string RegexPattern = "^[A-Z]{1}[a-zA-Z]{2,}";
+
+        public bool ValidateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, RegexPattern);
+        }
+    }
+}
